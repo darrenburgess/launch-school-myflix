98d62d9ac5efe3f79bc1eb492e81a256ded1088b
Darren Burgess <darrentburgess@gmail.com>
2017-08-28 14:32:22 -0400

    add tests for video validation

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe Video do
   it "saves itself" do
-    video = Video.new(title: "Some title", description: "Some description")
+    category = Category.create(name: "Some category")
+    video = Video.new(title: "Some title", description: "Some description", category: category)
     video.save
     expect(Video.first).to eq(video)
   end
@@ -11,5 +12,28 @@ describe Video do
     category = Category.create(name: "Some category")
     video = Video.create(title: "Some title", description: "the description", category: category)
     expect(video.category).to eq(category)
+  end
+
+  it "is valid with correct attributes" do
+    category = Category.create(name: "Some category")
+    video = Video.create(title: "Some title", description: "the description", category: category)
+    expect(video).to be_valid
+  end
+
+  it "is not valid without a title" do
+    category = Category.create(name: "Some category")
+    video = Video.new(title: nil, description: "the description", category: category)
+    expect(video).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    category = Category.create(name: "Some category")
+    video = Video.new(title: "sometitle", description: nil, category: category)
+    expect(video).to_not be_valid
+  end
+
+  it "is not valid without a category" do
+    video = Video.new(title: "sometitle", description: "the description", category: nil)
+    expect(video).to_not be_valid
   end
 end
