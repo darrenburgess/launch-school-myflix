c5c72724f9d7e02e42e9455a502fc8bfde78fa46
Darren Burgess <darrentburgess@gmail.com>
2017-08-29 09:36:48 -0400

    add search functionality to UI

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,17 +1,25 @@
 class VideosController < ApplicationController
   before_action :set_video, only: [:show]
+  before_action :set_categories, only: [:index]
 
   def index
-    @categories = Category.all
   end
 
   def show
+  end
+
+  def search
+    @videos = Video.search_by_title(params[:search_term]) 
   end
 
   private
 
   def set_video
     @video = Video.find(params[:id])
+  end
+
+  def set_categories
+    @categories = Category.all
   end
 
   def video_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Myflix::Application.routes.draw do
   root to: 'videos#index'
 
   get 'ui(/:action)', controller: 'ui'
-
   get 'home', to: "videos#index"
 
-  resources :videos, only: [:index, :show]
+  resources :videos, only: [:index, :show] do
+    collection do
+      post 'search'
+    end
+  end
+
   resources :categories, only: [:show]
 end
