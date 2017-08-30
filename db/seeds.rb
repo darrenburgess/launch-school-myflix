8232154d9969e0f9a02e1a295c277f81460fcf9c
Darren Burgess <darrentburgess@gmail.com>
2017-08-30 10:12:54 -0400

    add recent videos functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ unless Video.where(title: "Southpark").first
           description: "Sit minus cum accusantium voluptas cumque incidunt sapiente. Culpa ullam ad numquam in impedit, alias commodi quia totam optio maxime libero cupiditate. Harum similique non doloribus veniam delectus dignissimos ipsam.", 
           small_cover_url: "/tmp/south_park.jpg",
           large_cover_url: "/tmp/monk_large.jpg",
+          created_at: 7.days.ago,
           category: comedy
   )
 end
@@ -31,6 +32,7 @@ unless Video.where(title: "Family Guy").first
           description: "Sit minus cum accusantium voluptas cumque incidunt sapiente. Culpa ullam ad numquam in impedit, alias commodi quia totam optio maxime libero cupiditate. Harum similique non doloribus veniam delectus dignissimos ipsam.", 
           small_cover_url: "/tmp/family_guy.jpg",
           large_cover_url: "/tmp/monk_large.jpg",
+          created_at: 6.days.ago,
           category: comedy
   )
 end
@@ -41,6 +43,51 @@ unless Video.where(title: "Futurama").first
           description: "Sit minus cum accusantium voluptas cumque incidunt sapiente. Culpa ullam ad numquam in impedit, alias commodi quia totam optio maxime libero cupiditate. Harum similique non doloribus veniam delectus dignissimos ipsam.", 
           small_cover_url: "/tmp/futurama.jpg",
           large_cover_url: "/tmp/monk_large.jpg",
+          created_at: 5.days.ago,
+          category: comedy
+  )  
+end
+
+unless Video.where(title: "Freaks and Geeks").first
+  Video.create(
+          title: "Freaks and Geeks", 
+          description: "Sit minus cum accusantium voluptas cumque incidunt sapiente. Culpa ullam ad numquam in impedit, alias commodi quia totam optio maxime libero cupiditate. Harum similique non doloribus veniam delectus dignissimos ipsam.", 
+          small_cover_url: "/tmp/freaks_and_geeks.jpg",
+          large_cover_url: "/tmp/monk_large.jpg",
+          created_at: 4.days.ago,
+          category: comedy
+  )  
+end
+
+unless Video.where(title: "Marco Polo").first
+  Video.create(
+          title: "Marco Polo", 
+          description: "Sit minus cum accusantium voluptas cumque incidunt sapiente. Culpa ullam ad numquam in impedit, alias commodi quia totam optio maxime libero cupiditate. Harum similique non doloribus veniam delectus dignissimos ipsam.", 
+          small_cover_url: "/tmp/marco_polo.jpg",
+          large_cover_url: "/tmp/monk_large.jpg",
+          created_at: 3.days.ago,
+          category: comedy
+  )  
+end
+
+unless Video.where(title: "The Ranch").first
+  Video.create(
+          title: "The Ranch", 
+          description: "Sit minus cum accusantium voluptas cumque incidunt sapiente. Culpa ullam ad numquam in impedit, alias commodi quia totam optio maxime libero cupiditate. Harum similique non doloribus veniam delectus dignissimos ipsam.", 
+          small_cover_url: "/tmp/the_ranch.jpg",
+          large_cover_url: "/tmp/monk_large.jpg",
+          created_at: 2.days.ago,
+          category: comedy
+  )  
+end
+
+unless Video.where(title: "Vampire Diaries").first
+  Video.create(
+          title: "Vampire Diaries", 
+          description: "Sit minus cum accusantium voluptas cumque incidunt sapiente. Culpa ullam ad numquam in impedit, alias commodi quia totam optio maxime libero cupiditate. Harum similique non doloribus veniam delectus dignissimos ipsam.", 
+          small_cover_url: "/tmp/vampire_diaries.jpg",
+          large_cover_url: "/tmp/monk_large.jpg",
+          created_at: 1.days.ago,
           category: comedy
   )  
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,55 @@ require 'spec_helper'
 
 describe Category do
   it {should have_many(:videos)}
+
+  describe "#recent_vidoes" do
+    it "sorts the videos in descending order by created_at" do
+      comedy = Category.new(name: "Comedy")
+      3.times do |index|
+        Video.create(title: "Video #{index}", description: "description",
+                     category: comedy,
+                     created_at: index.days.from_now)
+      end
+
+      video1 = comedy.videos.first
+      video2 = comedy.videos.second
+      video3 = comedy.videos.third
+
+      expect(comedy.recent_videos).to eq([video1, video2, video3])
+    end
+
+    it "returns all videos if there are less than 6" do
+      comedy = Category.new(name: "Comedy")
+      3.times do |index|
+        Video.create(title: "Video #{index}", description: "description",
+                     category: comedy,
+                     created_at: index.days.from_now)
+      end
+
+      expect(comedy.recent_videos.count).to eq 3
+    end
+
+    it "returns 6 videos if there are more than 6" do
+      comedy = Category.new(name: "Comedy")
+      7.times do |index|
+        Video.create(title: "Video #{index}", description: "description",
+                     category: comedy,
+                     created_at: index.days.from_now)
+      end
+
+      expect(comedy.recent_videos.count).to eq 6
+    end
+
+    it "returns the most recent 6 videos" do
+      comedy = Category.new(name: "Comedy")
+      6.times{|index| Video.create(title: "Video #{index}", description: "d", category: comedy)}
+      older_video = Video.create(title: "title", description: "d", category: comedy, created_at:1.day.ago)
+      expect(comedy.recent_videos).to_not include(older_video)
+    end
+
+    it "returns an empty array if category has no videos" do
+      comedy = Category.new(name: "Comedy")
+      expect(comedy.recent_videos).to eq []
+    end
+  end
 end
