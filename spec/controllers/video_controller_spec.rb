6a8a66ce5fdd244ffa66c2fd230225d6ced621c5
Darren Burgess <darrentburgess@gmail.com>
2017-09-04 10:31:55 -0400

    add search tests to video controller

--- spec/controllers/video_controller_spec.rb
+++ spec/controllers/video_controller_spec.rb
@@ -16,20 +16,18 @@ describe VideosController do
     end
   end
 
-  describe "GET search" do
-    #context "with authenticated users" do
-      #before do
-        #session[:user_id] = Fabricate(:user).id
-      #end
+  describe "POST search" do
+    let(:video) { Fabricate :video }
 
-      #let(:video) { Fabricate :video }
+    it "sets the @videos variable given a valid search for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      get :search, search_term: video.title
+      expect(assigns(:videos)).to eq [video]
+    end
 
-      #it "sets the @videos variable given a valid search" do
-        #get :search, title: video.title
-        #expect(assigns(:video).should eq video)
-      #end
-
-      #it "renders the search template"
-    #end
+    it "sets the @videos variable given a valid search for authenticated users" do
+      get :search, search_term: video.title
+      expect(response).to redirect_to signin_path
+    end
   end
 end
